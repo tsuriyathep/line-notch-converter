6e5495c72d539d02cdae32a8a528f42e6e94fe87
tsuriyathep <tsuriyathep@users.noreply.github.com>
2023-01-24 18:05:55 -0800

    1 file modified, 1 added, 1 deleted

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import ReelConverter from './components/ReelConverter.vue';
+import LineNotchConverter from './components/LineNotchConverter.vue';
 </script>
 
 <template>
-  <h1>Reel Converter</h1>
-  <ReelConverter />
+  <h1>Line Notch Converter</h1>
+  <LineNotchConverter />
 </template>
 
 <style scoped></style>
--- src/components/LineNotchConverter.vue
+++ src/components/LineNotchConverter.vue
@@ -1,26 +1,33 @@
 <script setup>
 import { ref, computed, watch } from 'vue';
 
-let symbolsInOrder = ref('Wild,H1,H2,H3,H4,H5,L1,L2,L3,L4,L5,L6,Bonus,Replace');
-let reelText = ref(`Bonus	Bonus	Bonus	Bonus	Bonus
-L3	L3	L2	L4	L5
-H2	L3	H2	H2	H2`);
+let reelText = ref(`1	1	1	1	1
+2	2	2	2	2
+0	0	0	0	0
+3	3	3	3	3
+0	1	2	1	0
+3	2	1	2	3
+2	1	0	1	2
+1	2	3	2	1
+0	1	0	1	0
+3	2	3	2	3
+2	1	2	1	2
+1	2	1	2	1
+1	1	0	1	1
+2	2	3	2	2
+0	1	1	1	0
+3	2	2	2	3
+1	0	0	0	1
+2	3	3	3	2
+0	0	1	0	0
+3	3	2	3	3`);
 let reelResult = ref('');
-let symsArrStr = ref('');
 
 function process() {
   let result = '';
 
   // Clean input
-  symbolsInOrder.value = symbolsInOrder.value.trim().toUpperCase();
-  reelText.value = reelText.value.trim().toUpperCase();
-
-  // Get symbol indices
-  let symsArr = [];
-  if (symbolsInOrder.value.length > 0) {
-    symsArr = symbolsInOrder.value.split(',').map((n) => n.trim());
-    symsArrStr.value = symsArr.map((n, i) => `${i}=${n}`).join(',');
-  } else symsArrStr.value = '';
+  reelText.value = reelText.value.trim();
 
   // Get starting data row and count cols
   let rows = reelText.value.split('\n');
@@ -41,8 +48,6 @@ function process() {
     for (let i = rowStart; i < rows.length; i++) {
       let sym = rows[i].split('\t')[j];
       if (sym.length == 0) break;
-      let index = symsArr.indexOf(sym);
-      if (index > -1) sym = index; // Use symbol index of symbol, else string, if available
       colSymbols.push(sym);
     }
     result += `REEL#${j + 1}[${colSymbols.length}] = ${JSON.stringify(
@@ -53,7 +58,7 @@ function process() {
   reelResult.value = result;
 }
 
-watch([symbolsInOrder, reelText], (newValue, oldValue) => {
+watch([reelText], (newValue, oldValue) => {
   process();
 });
 
@@ -61,14 +66,6 @@ process();
 </script>
 
 <template>
-  <b>SYMBOLS (optional, in order, comma separated):</b>
-  <textarea v-model="symbolsInOrder" style="width: 100%" rows="3" />
-  <div
-    v-if="symsArrStr.length > 0"
-    style="margin-bottom: 8px; word-wrap: break-word"
-  >
-    {{ symsArrStr }}
-  </div>
   <b>INPUT (from Excel tab separated):</b>
   <textarea v-model="reelText" style="width: 100%" rows="20" />
   <b>OUTPUT (JSON array):</b>
